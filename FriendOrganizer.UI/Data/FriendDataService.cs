638a8ca5be131ba00bbf35975ce34e72d24c5396
Robin Edbom <robin.edbom@gmail.com>
2017-10-25 15:27:58 +0200

    Added viewmodels and dataAccesService

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend {FirstName = "Kalle", LastName = "Anka"};
+            yield return new Friend {FirstName = "Alexander", LastName = "Anka"};
+            yield return new Friend {FirstName = "Knatte", LastName = "Anka"};
+            yield return new Friend {FirstName = "Kajsa", LastName = "Anka"};
+        }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewMOdel:ViewModelBase
+    {
+        private readonly IFriendDataService _friendDataService;
+        private Friend _selecetedFriend;
+
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        public Friend SelecetedFriend
+        {
+            get => _selecetedFriend;
+            set
+            {
+                OnPropertyChanged();
+                _selecetedFriend = value;
+            }
+        }
+
+        public MainViewMOdel(IFriendDataService friendDataService)
+        {
+            _friendDataService = friendDataService;
+            Friends = new ObservableCollection<Friend>();
+        }
+
+        public void Load()
+        {
+            Friends.Clear();
+            foreach (var friend in _friendDataService.GetAll())
+            {
+                Friends.Add(friend);
+            }
+        }
+
+
+     
+    }
+}
